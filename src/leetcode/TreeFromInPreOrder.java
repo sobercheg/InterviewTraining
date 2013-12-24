36bd122ea259a95710520f7380a7293133c549b0
sobercheg <sobercheg@gmail.com>
2013-12-23 20:14:37 -0800

    Level by level tree print

--- src/leetcode/TreeFromInPreOrder.java
+++ src/leetcode/TreeFromInPreOrder.java
@@ -2,6 +2,9 @@ package leetcode;
 
 import common.TreeNode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Created by sobercheg on 12/22/13.
  */
@@ -53,9 +56,52 @@ public class TreeFromInPreOrder {
 
     public static void main(String[] args) {
         TreeFromInPreOrder treeFromInPreOrder = new TreeFromInPreOrder();
-        TreeNode root = treeFromInPreOrder.buildInorderPreorder(new int[]{7, 10, 4, 3, 1, 2, 8, 11}, new int[]{4, 10,
-                3, 1, 7, 11, 8, 2});
+        TreeNode root = treeFromInPreOrder.buildInorderPreorder(
+                new int[]{7, 10, 4, 3, 1, 2, 8, 11},
+                new int[]{4, 10, 3, 1, 7, 11, 8, 2});
+
+        printLevelByLevel(root);
 
     }
 
+    private static void printLevelByLevel(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<TreeNode>();
+        Queue<Integer> indent = new LinkedList<Integer>();
+        int counter1 = 0;
+        int counter2 = 0;
+        q.offer(root);
+        int initialIndent = 80;
+        indent.offer(initialIndent);
+        int prevInd = 0;
+        counter2++;
+        int level = 1;
+
+        while (!q.isEmpty()) {
+            TreeNode node = q.poll();
+            int ind = indent.poll();
+            System.out.print(String.format("%" + (ind - prevInd + 2) + "s", "{" + node + "}"));
+            prevInd = ind;
+
+            if (node.left != null) {
+                counter1++;
+                q.offer(node.left);
+                indent.offer(ind - initialIndent / (level + 1) + 4);
+            }
+
+            if (node.right != null) {
+                counter1++;
+                q.offer(node.right);
+                indent.offer(ind + initialIndent / (level + 1) - 4);
+            }
+
+            counter2--;
+            if (counter2 == 0) {
+                System.out.println();
+                counter2 = counter1;
+                counter1 = 0;
+                prevInd = 0;
+                level++;
+            }
+        }
+    }
 }
