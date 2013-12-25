a11910cb7a08c58e01ba44b594831668dfdc2f98
Sobercheg <sobercheg@gmail.com>
2013-12-24 21:18:18 -0800

    Reverse Polish Notation added

--- src/leetcode/oj/Solution.java
+++ src/leetcode/oj/Solution.java
@@ -1,6 +1,7 @@
 package leetcode.oj;
 
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Map;
 
 /**
@@ -10,7 +11,7 @@ import java.util.Map;
 public class Solution {
 
     /**
-     * http://oj.leetcode.com/problems/two-sum/
+     * <a href="http://oj.leetcode.com/problems/two-sum/">Two Sum</a>
      * <p/>
      * Problem: Given an array of integers, find two numbers such that they add up to a specific target number.
      * <p/>
@@ -35,4 +36,45 @@ public class Solution {
         return new int[]{-1, -1};
     }
 
+
+    /**
+     * <a href="http://oj.leetcode.com/problems/evaluate-reverse-polish-notation/">Evaluate Reverse Polish Notation</a>
+     * <p/>
+     * Problem: Evaluate the value of an arithmetic expression in Reverse Polish Notation.
+     * <p/>
+     * Solution:
+     * <pre>
+     * Let's use a Stack of operands. Iterate over the array of tokens and do the following:
+     *  - if the next token is an operand (number) put it to Stack
+     *  - else
+     *  - - pop last two Stack values
+     *  - - apply the token operator
+     *  - - push the result back to Stack
+     *  At the end Stack should have only one element which is the result. If Stack has 0 or more than 1 element the input expression was wrong.
+     * </pre>
+     * Limitation: only integers are used. So division operations will produce integers.
+     */
+    public int evalRPN(String[] tokens) {
+        LinkedList<Integer> stack = new LinkedList<Integer>();
+        for (String token : tokens) {
+            // Assume all tokens are valid: only numbers and the four operators are allowed (+, -, *, /)
+
+            // token is an operator
+            if ("+".equals(token)) {
+                stack.push(stack.pop() + stack.pop());
+            } else if ("-".equals(token)) {
+                int subtrahend = stack.pop();
+                stack.push(stack.pop() - subtrahend);
+            } else if ("*".equals(token)) {
+                stack.push(stack.pop() * stack.pop());
+            } else if ("/".equals(token)) {
+                int divisor = stack.pop();
+                stack.push(stack.pop() / divisor);
+            } else { // token is an operand
+                stack.push(Integer.parseInt(token));
+            }
+        }
+
+        return stack.pop();
+    }
 }
