7909ab222eb2a16f070a21b6fde48dfe959d1475
Sobercheg <sobercheg@gmail.com>
2014-01-05 22:41:58 -0800

    Search in Rotated Sorted Array added

--- src/leetcode/oj/Solution.java
+++ src/leetcode/oj/Solution.java
@@ -1898,6 +1898,38 @@ public class Solution {
         return longest;
     }
 
+    /**
+     * <a href="http://oj.leetcode.com/problems/search-in-rotated-sorted-array/">Search in Rotated Sorted Array</a>
+     * Suppose a sorted array is rotated at some pivot unknown to you beforehand.
+     * <p/>
+     * (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+     * <p/>
+     * You are given a target value to search. If found in the array return its index, otherwise return -1.
+     * <p/>
+     * You may assume no duplicate exists in the array.
+     */
+    public int search(int[] A, int target) {
+
+        int l = 0;
+        int r = A.length - 1;
+
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (A[mid] == target) return mid;
+
+            if (A[l] <= A[mid]) {
+                if (target >= A[l] && target < A[mid])
+                    r = mid - 1;
+                else l = mid + 1;
+            } else {
+                if (target > A[mid] && target <= A[r])
+                    l = mid + 1;
+                else r = mid - 1;
+            }
+        }
+
+        return -1;
+    }
 
 /************************** Data structures ****************************/
 
